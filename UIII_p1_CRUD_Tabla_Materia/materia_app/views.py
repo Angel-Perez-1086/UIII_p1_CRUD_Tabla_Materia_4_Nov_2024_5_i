b3d5fa5b57459331dd33089f630f5b468f532003
Angel Perez <164534035+Angel-Perez-1086@users.noreply.github.com>
2024-11-04 14:07:18 -0600

    Add files via upload
    crud version 1                 4 nov 2024

---
+++ UIII_p1_CRUD_Tabla_Materia/materia_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Materia
+# Create your views here.
+def inicio_vistas(request):
+    lasmaterias=Materia.objects.all()
+    return render(request,'gestionarMateria.html',{'materias':lasmaterias})
+
